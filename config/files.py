4e2c4c97d4a95c0636a1882d554c55eaed1074a0
Nikita Paramonov <nparamonov27@gmail.com>
2023-03-22 21:46:35 +0300

    Добавил модель для рендеринга HTML по шаблону

---
+++ config/files.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CURRENT_PATH = Path(__file__).absolute().parent.parent
+HTML_TEMPLATE_PATH = Path(CURRENT_PATH, 'templates')
--- routers/__init__.py
+++ routers/__init__.py
@@ -1,2 +1,1 @@
 from .common import router_common as common
-from .files import router_files as files
