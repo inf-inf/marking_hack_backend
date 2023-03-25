e540188674b9c3a34e004268401dc99a66a54efe
Nikita Paramonov <nparamonov27@gmail.com>
2023-03-25 15:09:32 +0300

    Добавил SQLAlchemy + psycopg2

---
+++ sql_db/orm_models/reports.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.dialects.postgresql import JSON
+
+from ..database import Base
+
+
+class Report(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(Integer)
+    data = Column(JSON, default=True)
