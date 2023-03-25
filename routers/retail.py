cef3983a0a3510ad3a452b6b5106f81a82744a77
Nikita Paramonov <nparamonov27@gmail.com>
2023-03-26 02:10:03 +0300

    Магазины get_stores из бд

--- routers/retail.py
+++ routers/retail.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from dependencies.sql_db import get_db
@@ -13,9 +14,9 @@ router_retail = APIRouter(
 
 def get_loss_rate(percent: float) -> str:
     """ Цвета для обозначения уровня потерь на карте """
-    if percent < 0:
+    if percent < -10:
         return '#28a745'
-    elif 0 <= percent < 3:
+    elif -10 <= percent <= 0:
         return '#ffc107'
     return '#dc3545'
 
@@ -23,40 +24,41 @@ def get_loss_rate(percent: float) -> str:
 @router_retail.get('/get_stores', summary='Получить торговые точки')
 def get_stores(inn: str = '', db: Session = Depends(get_db)):
 
+    sql = """
+        SELECT
+            s.id_sp,
+            s.region_code,
+            s.city_with_type,
+            s.city_fias_id,
+            s.postal_code,
+            SUM(
+                    (
+                        (current_date - rp.last_update_date) * rp.avg_cnt_per_day - rp.cnt
+                    ) * rp.price / rp.avg_month * 100
+                )  as loss_percentage
+        FROM public.stores AS s
+        JOIN public.retail_points AS rp ON s.id_sp = rp.id_sp
+        WHERE rp.avg_month > 0 AND s.inn = '6B8E111AB5B5C556C0AEA292ACA4D88B'
+        GROUP BY s.id_sp, s.region_code, s.city_with_type, s.city_fias_id, s.postal_code
+        ORDER BY loss_percentage DESC
+    """
+    stores = db.execute(text(sql)).all()
+    from random import randint
     res = [
         {
-            'id_sp': 'A44F290366CE1023507C993710313E3E',
-            'name': 'Такая себе точка',
-            'geo_lat': 55.787133,
-            'geo_lon': 37.686877,
-            'loss_percentage': 5,
-            'loss_rate': get_loss_rate(5)
-        },
-        {
-            'id_sp': 'D4079C72BE0EE138A787385DD327DF2E',
-            'name': 'Отличная точка',
-            'geo_lat': 55.741292,
-            'geo_lon': 37.415792,
-            'loss_percentage': -10,
-            'loss_rate': get_loss_rate(-10)
-        },
-        {
-            'id_sp': '95E5F1CB2478D04148F32E04509712B8',
-            'name': 'Быдло точка',
-            'geo_lat': 55.590990,
-            'geo_lon': 37.647963,
-            'loss_percentage': 11,
-            'loss_rate': get_loss_rate(11)
-        },
-        {
-            'id_sp': '44EF61711FD55ACC27AC2A767C655355',
-            'name': 'Вполне себе точка',
-            'geo_lat': 55.900998,
-            'geo_lon': 37.742439,
-            'loss_percentage': 0.5,
-            'loss_rate': get_loss_rate(0)
-        },
+            'id_sp': s.id_sp,
+            'region_code': s.region_code,
+            'city_with_type': s.city_with_type,
+            'city_fias_id': str(s.city_fias_id),
+            'postal_code': s.postal_code,
+            'name': 'Название точки',
+            'geo_lat': 55 + randint(450000, 780000) / 1000000,  # Временно рандомные координаты
+            'geo_lon': 37 + randint(450000, 780000) / 1000000,  # пока в базе нет реальных
+            'loss_percentage': float(s.loss_percentage),
+            'loss_rate': get_loss_rate(float(s.loss_percentage))
+        } for s in stores
     ]
+
     return JSONResponse(res)
 
 
