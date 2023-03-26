5e0c578c8f2795d34bad79b2dbf8573f5ac50b4d
KsQRooW <dimankochetov@yandex.ru>
2023-03-26 14:38:30 +0300

    Добавлены эндпоинты для загрузки csv файлов или отправки данных обычным POST методом. Также параметризирована реализация конфигурирования приложения.

---
+++ routers/upload_data.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, File, UploadFile
+from datetime import datetime
+from sqlalchemy.orm import Session
+from schemas.upload_data import RetailOut
+
+from dependencies.sql_db import get_db
+
+
+router_upload = APIRouter(
+    prefix='/upload',
+    tags=['upload']
+)
+
+
+@router_upload.post('/csv', summary='Загрузить csv файл с новыми данными')
+def save_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    file_name = f'data_{datetime.now()}.csv'
+    new_csv = open(f'../files/{file_name}', mode='w')
+    new_csv.write(file.read())
+    new_csv.close()
+    return {'file_name': file_name}
+
+
+@router_upload.post('/data')
+def save_data_like_csv(data: RetailOut):
+    file_name = f'data_{datetime.now()}.json'
+    new_csv = open(f'../files/{file_name}', mode='w')
+    new_csv.write(data.json(exclude_unset=True))
+    new_csv.close()
+    return {'file_name': file_name}
