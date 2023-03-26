b13216a6bdccaef3e52309cbacd9022797cc1f8e
Nikita Paramonov <nparamonov27@gmail.com>
2023-03-26 15:25:37 +0300

    Методы /upload мок

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,3 +1,5 @@
 from .common import router_common as common
 from .reports import router_reports as reports
 from .retail import router_retail as retail
+from .config import router_config as config
+from .upload_data import router_upload as upload
