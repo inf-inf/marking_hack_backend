eb0ef947e49222a70e7da5fbe02e30c1f9ef110e
Nikita Paramonov <nparamonov27@gmail.com>
2023-03-26 13:25:44 +0300

    [get_store_gtins] Данные из бд

---
+++ models/retail/stores.py
@@ -0,0 +1,17 @@
+
+def get_loss_rate(percent: float, light: bool = False) -> str:
+    """ Цвета для обозначения уровня потерь на карте """
+    if percent < -20:
+        return '#93ffac' * light or '#28a745'   # Зеленый
+    elif -20 <= percent <= -5:
+        return '#ffff9b' * light or '#ffc107'   # Желтый
+    return '#fc7462' * light or '#dc3545'       # Красный
+
+
+def get_days_overdue_msg(days_overdue: int) -> str:
+    """ Сообщение для менеджера о количестве дней до окончания товара """
+    if days_overdue > 0:
+        return f'Товар закончился {days_overdue} дн. назад'
+    elif days_overdue == 0:
+        return 'Товар закончится сегодня'
+    return f'Дней до окончания товара: {-days_overdue}'
